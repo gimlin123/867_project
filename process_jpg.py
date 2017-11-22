c3090e0e54ae060eab44b025790898ebe347a5ef
Mark Wang <mwang316@mit.edu>
2017-11-21 20:05:44 -0500

    adsfds

--- process_jpg.py
+++ process_jpg.py
@@ -17,13 +17,24 @@ def crop(bot_left, top_right, jpg_image):
 def openJPGImg(img_name):
     return Image.open(img_name)
 
-def create_neural_net(img_width, img_height):
+def create_neural_net(img_width, img_height, dropout_rate):
 
     model = Sequential()
     model.add(keras.layers.Conv2D(100, (50,50), strides=(20, 20), padding='valid', data_format=None, dilation_rate=(1, 1), activation=Activation("relu"),
     use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
     kernel_constraint=None, bias_constraint=None, input_shape = (img_width, img_height, 3)))
+    keras.layers.MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)
+
     model.add(keras.layers.Conv2D(50, (10,10), strides=(5, 5), padding='valid', data_format=None, dilation_rate=(1, 1), activation=Activation("relu"),
     use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
     kernel_constraint=None, bias_constraint=None))
+    keras.layers.MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)
+    model.add(keras.layers.flatten())
+    model.add(keras.layers.Dropout(dropout_rate, noise_shape=None, seed=None))
+    model.add(Dense(100, activation = "reLu"))
+    model.add(keras.layers.Dropout(dropout_rate, noise_shape=None, seed=None))
     model.add(Dense(26, activation='softmax'))
+
+def train_neural_net(neural_net):
+    model.compile(loss='binary_crossentropy', optimizer=SGD(lr = learning_rate), metrics=['accuracy'])
+    model.fit(trainX, one_hot_labels, epochs=1000, batch_size=100, verbose = 0)
