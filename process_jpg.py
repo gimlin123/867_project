677acc631ad78c3d7fb2846f675a0c2e75b7ec19
Mike Wang <mikewang@mit.edu>
2017-12-13 03:53:52 -0500

    slightly fixed

--- process_jpg.py
+++ process_jpg.py
@@ -1,9 +1,9 @@
 from PIL import Image
 import numpy as np
 import os
-# import keras
-# from keras.models import Sequential
-# from keras.layers import Conv2D, Activation, Dense
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense
 
 from os import walk
 import pickle
@@ -56,61 +56,79 @@ def create_training_dataset():
     max_width = 0
     max_height = 0
 
+    dict = {}
+    index = 0
+
 
     # 2036 and 2321
     # everything will be padded to size 2500 and 2500, since all pictures in training size should be less than this
     for (dirpath, dirnames, filenames) in walk(root):
-        for dirname in dirnames:
+        for i in range(24, 26):
             training_data = [[]] * 6
-            print dirname
-            x_sizes = []
-            y_sizes = []
+            label_arr = [[]] * 6
+            # print i
+            for dirname in dirnames:
+                # print dirname
+                x_sizes = []
+                y_sizes = []
 
-            for (dirpath2, dirnames2, filenames2) in walk(os.path.join(root, dirname)):
+                for (dirpath2, dirnames2, filenames2) in walk(os.path.join(root, dirname)):
 
-                for dirname2 in dirnames2:
+                    for dirname2 in dirnames2:
+                        dir_num = int(dirname2.split('_')[-1])
+                        if dir_num == 2*i or dir_num == 2*i + 1:
+                            for (dirpath3, dirnames3, filenames3) in walk(os.path.join(root, dirname, dirname2)):
+                                img_filename = ""
+                                meta_filename = ""
+                                for filename3 in filenames3:
+                                    if filename3.split("_")[-1] == 'rgb.jpg':
+                                        img_filename = filename3
+                                        meta_filename = img_filename[:-4] + '.json'
+                                        break
 
-                    if int(dirname2.split('_')[-1]) < 200:
-                        print dirname2
-                        for (dirpath3, dirnames3, filenames3) in walk(os.path.join(root, dirname, dirname2)):
-                            img_filename = ""
-                            meta_filename = ""
-                            for filename3 in filenames3:
-                                if filename3.split("_")[-1] == 'rgb.jpg':
-                                    img_filename = filename3
-                                    meta_filename = img_filename[:-4] + '.json'
-                                    break
+                                meta = json.load(open(os.path.join(root, dirname, dirname2, meta_filename)))
+                                bounding_box = meta['bounding_boxes'][0]['box']
+                                category = meta['bounding_boxes'][0]['category']
 
-                            meta = json.load(open(os.path.join(root, dirname, dirname2, meta_filename)))
-                            bounding_box = meta['bounding_boxes'][0]['box']
+                                img = openJPGImg(os.path.join(root, dirname, dirname2, img_filename))
+                                img_size = img.size
 
-                            img = openJPGImg(os.path.join(root, dirname, dirname2, img_filename))
-                            img_size = img.size
+                                img = crop((bounding_box[0], bounding_box[1]), (bounding_box[2], bounding_box[3]), img)
+                                img = resample_picture(meta['gsd'], 1.7, img)
+                                # num = int(dirname2.split('_')[-1])
+                #                 x_sizes.append((img.size[0], num))
+                #                 y_sizes.append((img.size[1], num))
 
-                            img = crop((bounding_box[0], bounding_box[1]), (bounding_box[2], bounding_box[3]), img)
-                            img = resample_picture(meta['gsd'], 1.7, img)
-                            # num = int(dirname2.split('_')[-1])
-            #                 x_sizes.append((img.size[0], num))
-            #                 y_sizes.append((img.size[1], num))
-            # print (min(x_sizes), max(x_sizes))
-            # print (min(y_sizes), max(y_sizes))
-                            # if img.size[0] > max_width:
-                            #     max_width = img.size[0]
-                            #
-                            # if img.size[1] > max_height:
-                            #     max_height = img.size[1]
-                            # print len(list(resized_img.getdata()))
-                            dim = max(img.size[0], img.size[1]) / 500
-                            padded_data = np.zeros(((dim + 1) * 500, (dim + 1) * 500, 3))
-                            padded_data[:img.size[1], :img.size[0] , :] = np.array(img.getdata()).reshape(img.size[1], img.size[0], 3)
-                            # training_data.append(np.array(img.getdata()).reshape(img.size[1], img.size[0], 3))
-                            # training_data.append(np.array(resized_img.getdata()).reshape(resized_img.size[1], resized_img.size[0], 3))
-                            training_data[dim].append(padded_data)
-                            # # print np.fromstring(resized_img.tobytes(), dtype=np.uint8)
+                                # if img.size[0] > max_width:
+                                #     max_width = img.size[0]
+                                #
+                                # if img.size[1] > max_height:
+                                #     max_height = img.size[1]
+                                # print len(list(resized_img.getdata()))
+                                dim = max(img.size[0], img.size[1]) / 500
+                                padded_data = np.zeros(((dim + 1) * 500, (dim + 1) * 500, 3))
+                                padded_data[:img.size[1], :img.size[0] , :] = np.array(img.getdata()).reshape(img.size[1], img.size[0], 3)
+                                # training_data.append(np.array(img.getdata()).reshape(img.size[1], img.size[0], 3))
+                                # training_data.append(np.array(resized_img.getdata()).reshape(resized_img.size[1], resized_img.size[0], 3))
+                                training_data[dim].append(padded_data)
+                                if category in dict:
+                                    vec = np.zeros(62)
+                                    vec[dict[category]] = 1
+                                    label_arr[dim].append(vec)
+                                else:
+                                    dict[category] = index
+                                    index += 1
+                                    vec = np.zeros(62)
+                                    vec[dict[category]] = 1
+                                    label_arr[dim].append(vec)
+                                # # print np.fromstring(resized_img.tobytes(), dtype=np.uint8)
+            if len(training_data[0]) > 0:
+                np.save("padded_data/resized_img_data_training_" + str(i), training_data)
+                np.save("padded_data_labels/resized_img_data_training_" + str(i), label_arr)
 
-            np.save("padded_data/resized_img_data_training_" + dirname, training_data)
+    # print dict
 
-create_training_dataset()
+# create_training_dataset()
 
 def make_training_matrices():
     root = './processed_data'
@@ -134,8 +152,8 @@ def make_training_matrices():
                 label = [0] * len(dict)
                 label[dict[filename.split('_')[-1]]] = 1
                 labels[i] += [label] * len(loaded_data[i])
-            print training_data
-            print labels
+            # print training_data
+            # print labels
 
         # training_data = np.array(training_data)
         # training_labels = np.zeros((len(labels), len(dict)))
@@ -144,36 +162,57 @@ def make_training_matrices():
 
         np.save('training_data', training_data)
         np.save('training_labels', training_labels)
-        print dict
+        # print dict
 
-
+# test = np.load('padded_data/resized_img_data_training_airport_terminal.npy')
+# print test[0][0].shape
 # make_training_matrices()
 # a = np.load('resized_img_data_training_airport.npy')
 # print a.shape
 # #
 
-# model = Sequential()
-# model.add(keras.layers.Conv2D(5, (3,3), strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1), activation=Activation("relu"),
-# use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
-# kernel_constraint=None, bias_constraint=None, input_shape = (None, None, 3)))
-# model.add(keras.layers.Conv2D(5, (3,3), strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1), activation=Activation("relu"),
-# use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
-# kernel_constraint=None, bias_constraint=None))
-# model.add(Dense(62, activation='softmax'))
+model = Sequential()
+model.add(keras.layers.Conv2D(5, (3,3), strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1), activation="relu",
+use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
+kernel_constraint=None, bias_constraint=None, input_shape = (500, 500, 3)))
+model.add(keras.layers.Conv2D(5, (3,3), strides=(1, 1), padding='valid', data_format=None, dilation_rate=(1, 1), activation="relu",
+use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
+kernel_constraint=None, bias_constraint=None))
+model.add(keras.layers.Conv2D(5, (5,5), strides=(2, 2), padding='valid', data_format=None, dilation_rate=(1, 1), activation="relu",
+use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None,
+kernel_constraint=None, bias_constraint=None))
+keras.layers.MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None)
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dropout(0.5, noise_shape=None, seed=None))
+model.add(Dense(100, activation = "relu"))
+model.add(keras.layers.Dropout(0.5, noise_shape=None, seed=None))
+model.add(Dense(62, activation='softmax'))
 #
-# # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+for j in range(3):
+    for i in range(50):
+        x_train = np.load('padded_data/' + 'resized_img_data_training_' + str(i) + '.npy', encoding='latin1')[0]
+        y_train = np.load('padded_data_labels/' + 'resized_img_data_training_' + str(i) + '.npy')[0]
+
+        y_train_small = np.array([y_train[i] for i in range(len(x_train)) if x_train[i].shape[0] == 500])
+        x_train_small = np.array([arr for arr in x_train if arr.shape[0] == 500])
+
+        print(y_train_small.shape)
+        print(x_train_small.shape)
+        model.fit(x_train_small, y_train_small,
+                  batch_size=5,
+                  epochs=1,
+                  verbose=1)
+        model.save('trained_model_1.h5')
 # x_train = np.load('training_data.npy', encoding='latin1')
 # # print(x_train)
 # y_train = np.load('training_labels.npy')
 # print(len(x_train))
 # print(x_train[0])
 # print(x_train[0].shape)
-# model.fit(x_train, y_train,
-#           batch_size=50,
-#           epochs=3,
-#           verbose=1)
+
 #
 # score = model.evaluate(x_train, y_train, verbose=0)
-# model.save('trained_model_1')
+
 
 # create_training_dataset()
